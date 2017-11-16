85bc5322453876643ece0a449306d063b0c94797
DESKTOP-Q2MP1SP\-Unknown- <torrinmartel@gmail.com>
2017-11-16 03:01:50 -0700

    2

---
+++ Project-Platformer/Assets/Scripts/Player/MoveCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+
+	public Transform Target;
+	public float SmoothSpeed = 10f;
+	public Vector3 Offset;
+
+	private Camera MainCamera;
+
+	void Start () {
+		//ID
+		MainCamera = FindObjectOfType<Camera>();
+	}
+
+	void FixedUpdate () {
+		//Vectors
+		Vector3 DesiredPosition = Target.position + Offset;
+		Vector3 SmoothedPosition = Vector3.Lerp (transform.position,DesiredPosition,SmoothSpeed * Time.deltaTime);
+		//Move Camera
+		transform.position = SmoothedPosition;
+	}
+}
---
+++ Project-Platformer/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	
+	public float MoveSpeed;
+
+	private Rigidbody Rb;
+	private Vector3 MoveInput;
+	private Vector3 MoveVelocity;
+
+	void Start () {
+		//ID
+		Rb = GetComponent<Rigidbody>();
+	}
+
+	void Update () {
+		//Get Input
+		MoveInput = new Vector3(Input.GetAxisRaw("Key_Horizontal"),0f,0f) * MoveSpeed;
+	}
+
+	void FixedUpdate () {
+		//Move
+		MoveInput.y = Rb.velocity.y;
+		MoveVelocity = MoveInput;
+		Rb.velocity = MoveVelocity;
+	}
+}
+

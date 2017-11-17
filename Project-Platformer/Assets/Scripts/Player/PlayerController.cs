74084f55c88571788f342ee416ad42837f0d342a
DESKTOP-Q2MP1SP\-Unknown- <torrinmartel@gmail.com>
2017-11-16 20:07:48 -0700

    2.3

--- Project-Platformer/Assets/Scripts/Player/PlayerController.cs
+++ Project-Platformer/Assets/Scripts/Player/PlayerController.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
 	
 	public float MoveSpeed;
+	public float JumpHeight;
 
 	private Rigidbody Rb;
 	private Vector3 MoveInput;
@@ -18,6 +20,9 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
 		//Get Input
 		MoveInput = new Vector3(Input.GetAxisRaw("Key_Horizontal"),0f,0f) * MoveSpeed;
+		if (transform.position.y <= -20) {
+			SceneManager.LoadScene ("Debug",LoadSceneMode.Single);
+		}
 	}
 
 	void FixedUpdate () {
@@ -25,6 +30,10 @@ public class PlayerController : MonoBehaviour {
 		MoveInput.y = Rb.velocity.y - 1;
 		MoveVelocity = MoveInput;
 		Rb.velocity = MoveVelocity;
+		//Jump (Detect For Ground Will Be Done Later!)
+		if (Input.GetButton("Key_Jump")) {
+			Rb.AddForce(new Vector2(0f, JumpHeight));
+		}
 	}
 }
 
